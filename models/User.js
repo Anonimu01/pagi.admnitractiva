c78dfa38299f2a5fa7898fe40d5032fc3f33216c
Anonimu01 <149401347+Anonimu01@users.noreply.github.com>
2026-05-22 16:46:54 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -6,35 +6,28 @@ const UserSchema = new mongoose.Schema({
     required: true,
     unique: true
   },
-
   password: {
     type: String,
     required: true
   },
-
   balance: {
     type: Number,
     default: 0
   },
-
   leverage: {
     type: Number,
     default: 1
   },
-
-  // 🔥 IMPORTANTE para control y tiempo real
   isOnline: {
     type: Boolean,
     default: false
   },
-
   lastLogin: {
     type: Date,
     default: null
   }
-
 }, {
-  timestamps: true // 👈 agrega createdAt y updatedAt (IMPORTANTE)
+  timestamps: true
 });
 
 module.exports = mongoose.model("User", UserSchema);
