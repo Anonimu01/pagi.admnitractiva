3ba10f85b37763faf60b84ac468b220ad56d8aea
Anonimu01 <149401347+Anonimu01@users.noreply.github.com>
2026-05-22 16:48:50 -0700

    Update Wallet.js

--- models/Wallet.js
+++ models/Wallet.js
@@ -1,20 +1,22 @@
 const mongoose = require("mongoose");
 
 const walletSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
 
   balanceOwn: { type: Number, default: 0 },
   balance: { type: Number, default: 0 },
+  equity: { type: Number, default: 0 },
 
   credit: { type: Number, default: 0 },
   marginUsed: { type: Number, default: 0 },
+  freeMargin: { type: Number, default: 0 },
+  marginLevel: { type: Number, default: 0 },
 
   leverageFactor: { type: Number, default: 1 },
+  currency: { type: String, default: "USD" }
 
-  currency: { type: String, default: "USD" },
-
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+}, {
+  timestamps: true // Esto automáticamente crea createdAt y updatedAt
 });
 
 module.exports = mongoose.model("Wallet", walletSchema);
