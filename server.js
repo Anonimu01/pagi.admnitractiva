e6332f3457fd9825e3dc56b7d4f116b103ab2f66
Anonimu01 <149401347+Anonimu01@users.noreply.github.com>
2026-06-03 07:21:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3518,7 +3518,7 @@ app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 /* =========================
    FILES (IMPORTANTE)
